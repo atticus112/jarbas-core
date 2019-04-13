7576fd94c5be525020a4f554c810aae58a1878db
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2019-04-13 14:50:46 +0100

    Feat/converse deactivated handler (#9)
    * converse deactivated handler

--- mycroft/skills/core.py
+++ mycroft/skills/core.py
@@ -566,6 +566,7 @@ class MycroftSkill:
                            self.handle_set_cross_context)
             self.add_event("mycroft.skill.remove_cross_context",
                            self.handle_remove_cross_context)
+            self.add_event("converse.deactivate", self._deactivate_skill)
             name = 'mycroft.skills.settings.update'
             func = self.settings.run_poll
             bus.on(name, func)
@@ -597,7 +598,18 @@ class MycroftSkill:
         """
         return None
 
-    def converse(self, utterances, lang=None):
+    def _deactivate_skill(self, message):
+        skill_id = message.data.get("skill_id")
+        if skill_id == self.skill_id:
+            self.on_deactivate()
+
+    def on_deactivate(self):
+        """
+        Invoked when the skill is removed from active skill list
+        """
+        pass
+
+    def converse(self, utterances, lang="en-us"):
         """ Handle conversation.
 
         This method gets a peek at utterances before the normal intent
